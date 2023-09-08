bdae9f78cc5767995d49d6b8675e71a2b41c682f
calee14 <caplee1000@gmail.com>
2023-09-07 18:40:21 -0700

    test sd upscaler

--- src/stable_diff.py
+++ src/stable_diff.py
@@ -4,6 +4,7 @@ from diffusers import StableDiffusionUpscalePipeline
 import torch
 from io import BytesIO
 from PIL import Image
+import numpy as np
 
 video_path = './media/name.mp4'
 
@@ -14,26 +15,37 @@ pipeline = pipeline.to("cuda")
 
 cap = cv2.VideoCapture(video_path)
 
-cap.set(cv2.CAP_PROP_POS_FRAMES, 200)
+cap.set(cv2.CAP_PROP_POS_FRAMES, 250)
 success, frame = cap.read()
-if success:
-    # use the super resolution model to upscale the image, timing how
-    # long it takes
-    small_frame = cv2.resize(frame, (frame.shape[1]//2, frame.shape[0]//2))
-    byte_data = BytesIO(small_frame)
-    small_frame = Image.open(small_frame).convert("RGB")
-    start = time.time()
-    prompt = "hot sexy 4k"
-    upscaled = pipeline(prompt=prompt, image=frame).images[0]
-    end = time.time()
-    print("[INFO] super resolution took {:.6f} seconds".format(
-	end - start))
-    cv2.imshow('Upscaled Frame', upscaled)
-    cv2.imshow('Original Frame', frame)
-    k = cv2.waitKey(10000)
 
-    if k == 27:         # If escape was pressed exit
-        cv2.destroyAllWindows()
+if success:
+    with torch.no_grad():
+        # use the super resolution model to upscale the image, timing how
+        # long it takes
+        small_frame = cv2.resize(frame, (frame.shape[1]//6, frame.shape[0]//6))
+        img_array = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+        print('IMAGE DIM:', img_array.shape)
+        small_frame = Image.fromarray(img_array, 'RGB').convert('RGB')
+
+        # run x4 upscaler
+        start = time.time()
+        prompt = "realistic, sharp, vibrant, smooth"
+        upscaled = pipeline(prompt=prompt, image=small_frame).images[0]
+        end = time.time()
+        print("[INFO] super resolution took {:.6f} seconds".format(
+        end - start))
+
+        # show results
+        upscaled.save('test.png')
+        print('UPSCALED SIZE:', upscaled.size)
+        cv2.imwrite('original.png', frame)
+        # cv2.imshow('Upscaled Frame', upscaled)
+        # cv2.imshow('Original Frame', frame)
+        # k = cv2.waitKey(10000)
+
+        # if k == 27:         # If escape was pressed exit
+        #     cv2.destroyAllWindows()
+
 # while True:
 #     success, frame = cap.read()
 #     if success:
