df63ed9db03177fb681901c925b77cb8dc5b98f6
calee14 <caplee1000@gmail.com>
2023-09-16 02:36:22 -0700

    added code to copy audio

--- test/audio.py
+++ test/audio.py
@@ -8,11 +8,16 @@ from src.util import img_processing as imgp
 from src.util import cv_processing as cvp
 from src.util import pil_processing as pilp
 from PIL import Image, ImageFilter, ImageEnhance
-from moviepy import AudioFileClip, VideoFileClip
+from moviepy.editor import AudioFileClip, VideoFileClip
 
-video_path = './media/name.mp4'
+video_path = '../media/name.mp4'
+target_vid_path = '../name_remastered.mp4'
 
 audio = AudioFileClip(video_path)
-print(audio)
+target_vid = VideoFileClip(target_vid_path)
 
+target_vid.audio = audio
+target_vid.write_videofile(target_vid_path, codec='libx264', audio_codec='aac')
+
+target_vid.close()
 audio.close()
